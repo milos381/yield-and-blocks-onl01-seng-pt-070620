9cbbf0de1a270908afe8e58ef5569d5d0262f7ce
Milos Popovic <milos_popovic@outlook.com>
2020-07-10 02:30:14 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+def hello_t(array)
+  if block_given?
+  i = 0
 
+while i < array.length
+  yield (array[i]) #Here, we use the bracket ([]) method to grab the value
+                #of each successive index element as we proceed
+                # through our while loop, yielding each to a block
+  i += 1
+end
+array
+else
+  puts "Hey! No block was given!"
+end
 end
 
 # call your method here!
-
