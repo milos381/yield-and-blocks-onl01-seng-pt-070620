beb7bc83a84bc95e8a609b82ec357eb51cf0ebc7
Milos Popovic <milos_popovic@outlook.com>
2020-07-10 01:48:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+def hello_t(array)
+  i = 0
 
+while i < array.length
+  yield array[i] #Here, we use the bracket ([]) method to grab the value 
+                #of each successive index element as we proceed
+                # through our while loop, yielding each to a block
+  i += 1
+end
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
