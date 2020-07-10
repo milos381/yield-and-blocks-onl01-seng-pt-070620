ac07d719dddf440fafcb25e8304e53e2c94fb52c
Milos Popovic <milos_popovic@outlook.com>
2020-07-10 02:26:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array)
   i = 0
 
 while i < array.length
-  yield array[i] #Here, we use the bracket ([]) method to grab the value
+  yield (array[i]) #Here, we use the bracket ([]) method to grab the value
                 #of each successive index element as we proceed
                 # through our while loop, yielding each to a block
   i += 1
